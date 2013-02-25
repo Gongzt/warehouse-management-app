1b6692d82ad8e12360b016504805192110147d55
zhitaogong@googlemail.com <zhitaogong@googlemail.com@a0e4ae1c-28e5-5f8f-adc8-8227751d5b94>
2013-02-25 06:52:21 +0000

    Gongzt - created a tabhost

--- gen/com/android/app/warehousemanagement/R.java
+++ gen/com/android/app/warehousemanagement/R.java
@@ -24,7 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int tab1=0x7f080000;
+        public static final int tab2=0x7f080001;
+        public static final int tab3=0x7f080002;
+        public static final int tab4=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,7 +39,6 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
--- src/com/android/app/warehousemanagement/MainActivity.java
+++ src/com/android/app/warehousemanagement/MainActivity.java
@@ -3,6 +3,8 @@ package com.android.app.warehousemanagement;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		setupTabHost(0);
 	}
 
 	@Override
@@ -18,5 +22,31 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+    public void setupTabHost(int currentTabNum) {
+	    TabHost tabHost = (TabHost)findViewById(android.R.id.tabhost);
+	    tabHost.setup();
+	    
+	    TabHost.TabSpec tab1 = tabHost.newTabSpec("tab1").setContent(R.id.tab1).setIndicator("库存现况");
+	    TabHost.TabSpec tab2 = tabHost.newTabSpec("tab2").setContent(R.id.tab2).setIndicator("入库");
+	    TabHost.TabSpec tab3 = tabHost.newTabSpec("tab3").setContent(R.id.tab3).setIndicator("历史记录");
+	    TabHost.TabSpec tab4 = tabHost.newTabSpec("tab4").setContent(R.id.tab4).setIndicator("设置");
+	    
+	    tabHost.addTab(tab1);
+	    tabHost.addTab(tab2);
+	    tabHost.addTab(tab3);
+	    tabHost.addTab(tab4);
+	    
+	    tabHost.setCurrentTab(currentTabNum);
+	    tabHost.setOnTabChangedListener(new TabHostOnTabChangedListener());
+    }
 
+}
+
+class TabHostOnTabChangedListener implements OnTabChangeListener {
+	
+	@Override
+	public void onTabChanged(String tabId) {
+		// TODO Auto-generated method stub
+	}
 }
