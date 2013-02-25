d9cfabc6521aa2661dee638aa56bb74c8e21b33a
zhitaogong@googlemail.com <zhitaogong@googlemail.com@a0e4ae1c-28e5-5f8f-adc8-8227751d5b94>
2013-02-25 05:18:26 +0000

    Gongzt - Uploaded a new project

---
+++ gen/com/android/app/warehousemanagement/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.android.app.warehousemanagement;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
