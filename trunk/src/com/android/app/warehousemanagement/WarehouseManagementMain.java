9e5e59e4e082a567c39f8c5d310b135176b139f1
zhitaogong@googlemail.com <zhitaogong@googlemail.com@a0e4ae1c-28e5-5f8f-adc8-8227751d5b94>
2013-03-02 05:38:08 +0000

    Gongzt - Minor changes

--- trunk/src/com/android/app/warehousemanagement/WarehouseManagementMain.java
+++ trunk/src/com/android/app/warehousemanagement/WarehouseManagementMain.java
@@ -39,16 +39,16 @@ public class WarehouseManagementMain extends FragmentActivity {
 	    tabHost.setCurrentTab(currentTabNum);
 	    
 		tabManager.addTab(
-				tabHost.newTabSpec("Fragment1").setIndicator("库存现状"),
+				tabHost.newTabSpec("CurrentFragment").setIndicator("库存现状"),
 				CurrentFragment.class, null);
 		tabManager.addTab(
-				tabHost.newTabSpec("Fragment2").setIndicator("入库"),
+				tabHost.newTabSpec("InStockFragment").setIndicator("入库"),
 				InStockFragment.class, null);
 		tabManager.addTab(
-				tabHost.newTabSpec("Fragment3").setIndicator("历史"),
+				tabHost.newTabSpec("RecordFragment").setIndicator("历史"),
 				RecordFragment.class, null);
 		tabManager.addTab(
-				tabHost.newTabSpec("Fragment4").setIndicator("设置"),
+				tabHost.newTabSpec("SettingFragment").setIndicator("设置"),
 				SettingFragment.class, null);
     }
 
