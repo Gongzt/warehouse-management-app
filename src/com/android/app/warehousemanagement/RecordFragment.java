39f3416dce70748f48c2e19277acab71257302e0
tidussea <tidussea@gmail.com>
2013-03-07 07:03:03 +0000

    add ListView and search function

--- src/com/android/app/warehousemanagement/RecordFragment.java
+++ src/com/android/app/warehousemanagement/RecordFragment.java
@@ -1,22 +1,88 @@
 package com.android.app.warehousemanagement;
 
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
-public class RecordFragment extends Fragment{
+import com.android.app.warehousemanagement.db.InnerDBExec;
+import com.android.app.warehousemanagement.db.InnerDBTable;
+
+public class RecordFragment extends ListFragment 
+							implements TextWatcher{
+	
+	private InnerDBExec db = null;
+	private SimpleCursorAdapter mAdapter = null;
+	private String[] fromColumns = new String[] {
+    		InnerDBTable.Record.COLUMN_NAME_DATE,
+    		InnerDBTable.Record.COLUMN_NAME_ENTRY_TYPE,
+    		InnerDBTable.Record.COLUMN_NAME_ENTRY_NAME,
+    		InnerDBTable.Record.COLUMN_NAME_INOROUT,
+    		InnerDBTable.Record.COLUMN_NAME_AMOUNT,
+    		InnerDBTable.Record.COLUMN_NAME_STATUS
+    };
+	private int[] toViews = new int[] {
+    		R.id.recordDateTextView,
+    		R.id.recordTypeTextView,
+    		R.id.recordEntryTextView,
+    		R.id.recordInoroutTextView,
+    		R.id.recordAmountTextView,
+    		R.id.recordStatusTextView
+    };
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        db = new InnerDBExec(getActivity());
+        
+        mAdapter= new SimpleCursorAdapter(getActivity(),
+        		R.layout.record_list_item,
+        		db.recordSelectAll(),
+        		fromColumns,
+        		toViews,
+        		0);
+        setListAdapter(mAdapter);
+        
+        return inflater.inflate(R.layout.record_fragment, container, false);
     }
+    
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+    	
+    	super.onViewCreated(view, savedInstanceState);
+    	
+    	EditText editText = (EditText)getView().findViewById(R.id.recordSearchText);
+        editText.addTextChangedListener(this);
+    }
+    
+    @Override
+    public void afterTextChanged(Editable s) {
+
+	}
+    
+    @Override
+	public void beforeTextChanged(CharSequence s, int start, int count,
+			int after) {
+
+	}
+
+    @Override
+	public void onTextChanged(CharSequence s, int start, int before,
+			int count) {
+		mAdapter.swapCursor(db.recordSearch(s.toString()));
+
+	}
+
 }
--- src/com/android/app/warehousemanagement/db/InnerDBTable.java
+++ src/com/android/app/warehousemanagement/db/InnerDBTable.java
@@ -9,7 +9,7 @@ public abstract class InnerDBTable {
 	
 	public static abstract class Current implements BaseColumns{
 		public static final String TABLE_NAME = "Current";
-		public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+		public static final String COLUMN_NAME_ENTRY_ID = "_id";
 		public static final String COLUMN_NAME_ENTRY_NAME = "entryname";
 		public static final String COLUMN_NAME_WAREHOUSE = "warehouse";
 		public static final String COLUMN_NAME_AMOUNT = "amount";
@@ -19,7 +19,7 @@ public abstract class InnerDBTable {
 	
 	public static abstract class Record implements BaseColumns{
 		public static final String TABLE_NAME = "Record";
-		public static final String COLUMN_NAME_RECORD_ID = "recordid";
+		public static final String COLUMN_NAME_RECORD_ID = "_id";
 		public static final String COLUMN_NAME_DATE = "date";
 		public static final String COLUMN_NAME_ENTRY_NAME = "entryname";
 		public static final String COLUMN_NAME_ENTRY_TYPE = "type";
